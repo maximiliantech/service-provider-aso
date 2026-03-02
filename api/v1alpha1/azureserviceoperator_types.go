284abfa9259ec4ebc691497a2aa9b942011b8a91
Maximilian Techritz <maximilian.techritz@sap.com>
2026-03-02 11:51:34 +0100

    feat: add version field to AzureServiceOperator API

--- api/v1alpha1/azureserviceoperator_types.go
+++ api/v1alpha1/azureserviceoperator_types.go
@@ -20,19 +20,10 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // AzureServiceOperatorSpec defines the desired state of AzureServiceOperator
 type AzureServiceOperatorSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-	// The following markers will use OpenAPI v3 schema to validate the value
-	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
-
-	// foo is an example field of AzureServiceOperator. Edit azureserviceoperator_types.go to remove/update
-	// +optional
-	Foo *string `json:"foo,omitempty"`
+	// Version is the version of the Azure Service Operator
+	Version string `json:"version"`
 }
 
 // AzureServiceOperatorStatus defines the observed state of AzureServiceOperator.
